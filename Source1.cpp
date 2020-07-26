a5ca59eb805cb65cb44072a38696a48655b03004
HECTOP35 <39101762+HECTOP35@users.noreply.github.com>
2020-07-26 09:55:31 +0400

    2

--- Source1.cpp
+++ Source1.cpp
@@ -1,19 +1,52 @@
 #include <iostream>
-
+#include <fstream>
+#include <algorithm>
+#include <stdlib.h>
 using namespace std;
 
-int main() {
-    double value = 0;
-
-
-    cout << "input value: ";        //Диалог: введите число
-
-    while (!(cin >> value)) {        //пока не будет введено нормальное число, выполняем цикл
-        cout << "error\n";          //сообщаем об ошибке ввода
-        cin.clear();                //сбрасываем коматозное состояние cin
-        fflush(stdin);              //очищаем поток ввода
+void randomF() {
+    int leght = 30;
+    int max_number = 100;
+    fstream f;
+    f.open("input.txt", ios::out);
+    for (int i = 0; i < leght; i++) {
+        f << 1 + rand() % ((max_number + 1) - 1) << " ";
     }
 
-    cout << "value = " << value << '\n';
-    cin.get();
+}
+
+int main()
+{
+    randomF();
+    int* array;
+    fstream f;
+    f.open("input.txt", ios::in);
+    int i;
+    int size = 0;
+
+    while (!f.eof() && f >> i)
+    {
+        size++;
+    }
+    array = new int[size];
+    f.close();
+
+    f.open("input.txt", ios::in);
+    for (int i = 0; i < size; i++)
+    {
+        f >> array[i];
+    }
+    f.close();
+
+    sort(array, array + (size));
+    f.open("output.txt", ios::out);
+    for (int i = 0; i < size; i++)
+    {
+        f << array[i] << " ";
+    }
+
+    f.close();
+    delete[]array;
+
+    return 0;
 }
